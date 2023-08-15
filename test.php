0dbe83691fabd74522c966a58743c8eda69df8a1
eocnd1116 <68635662+eocnd1116@users.noreply.github.com>
2023-08-15 18:34:39 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>권준 천재</title>
+    <script src="js/example.js"></script>
+</head>
+<body>
+    <script>
+        document.write("fuck");
+    </script>
+    <?php
+        echo "와 샌즈"
+    ?>
+</body>
+</html>
